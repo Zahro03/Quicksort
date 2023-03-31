c2f3b33e2a284ad8b1590185f626053518f5266c
Zahro03 <127277232+Zahro03@users.noreply.github.com>
2023-03-31 16:06:15 +0700

    //swap the element at index x with the element at index y

--- Quicksort/Quicksort.cpp
+++ Quicksort/Quicksort.cpp
@@ -63,7 +63,7 @@ void q_short(int low, int high)
 			cmp_count++;
 		}
 		cmp_count++;
-		//search for an element greater than pivot
+		//search for an element greater than pivot 
 		while ((arr[i] <= pivot) && (j >= low)) // langkah 7
 		{
 			j--; //langkah 8
